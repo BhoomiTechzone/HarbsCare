4311137db6848191886a289c6039ed80b1ec0820
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-09-16 17:59:52 +0530

    nav-Section

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+
 
 const TopNavbar = () => {
   return (
-    <Card style={{  }}>
-      <ListGroup variant="flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      </ListGroup>
-    </Card>
+    <div style={{backgroundColor: "red", display:"flex", justifyContent:"space-between", alignItems:"center", padding:"0px 2rem"}}>
+      <p style={{marginBottom:"0px", fontSize:"10px", fontWeight:"bold"}}>India' 1st Brand with MADE SAFE Certified Products</p>
+      <p style={{marginBottom:"0px", fontSize:"10px", fontWeight:"bold"}}>OMG Sale is Live | Buy 1 Get 1 FREE | Coupon:OMG | Click To Shop</p>
+      <p style={{marginBottom:"0px", fontSize:"10px", fontWeight:"bold"}}>ff</p>
+    </div>
   )
 }
 
